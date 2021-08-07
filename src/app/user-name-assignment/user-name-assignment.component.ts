da99aa1596a1de030003a41bfe57b288c3444ea9
behrami <behram1993@gmail.com>
2021-08-07 13:47:26 -0400

    added a username field that updates text as you type in. added reset button that resets username and is only enabled when a username is typed in.

--- src/app/user-name-assignment/user-name-assignment.component.ts
+++ src/app/user-name-assignment/user-name-assignment.component.ts
@@ -3,13 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-user-name-assignment',
   templateUrl: './user-name-assignment.component.html',
-  styleUrls: ['./user-name-assignment.component.css']
+  styleUrls: ['./user-name-assignment.component.css'],
 })
 export class UserNameAssignmentComponent implements OnInit {
+  userName = '';
+  resetDisabled = true;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onResetUsername() {
+    this.userName = '';
   }
 
+  isDisabled() {
+    if (this.userName === '') {
+      this.resetDisabled = true;
+    } else {
+      this.resetDisabled = false;
+    }
+    return this.resetDisabled;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
