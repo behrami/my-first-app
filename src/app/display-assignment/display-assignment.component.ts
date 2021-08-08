1ac00a4ee53faf027177387d24182a1a82cb6ba1
behrami <behram1993@gmail.com>
2021-08-08 14:58:46 -0400

    fixed background color issue for the assignment

--- src/app/display-assignment/display-assignment.component.ts
+++ src/app/display-assignment/display-assignment.component.ts
@@ -7,15 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DisplayAssignmentComponent implements OnInit {
   details =
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas aliquet mauris lorem, ac viverra libero fermentum non. Sed convallis lectus at ex posuere, at vulputate mi ultrices. Vestibulum bibendum justo in ipsum egestas, a pulvinar lorem fermentum. Proin vel euismod sem. Nulla fermentum sodales lorem, fermentum aliquam elit blandit a. Vestibulum gravida sagittis odio. Aenean egestas condimentum pretium. Curabitur vel felis dapibus, vehicula massa eu, ornare ex. Etiam consequat tellus vel ligula suscipit iaculis. ';
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas aliquet mauris lorem, ac viverra libero fermentum non. Sed convallis lectus at ex posuere, at vulputate mi ultrices. ';
   displayBtn = true;
   dispayDetails = false;
   btnClicks = [];
-  numClicks = 0;
 
   addTimestamp() {
     this.btnClicks.push(Date().toLocaleString());
-    this.numClicks++;
   }
 
   onDisplayClick() {
